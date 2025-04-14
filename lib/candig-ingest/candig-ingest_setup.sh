622331c59b492b5972976c329e326f644185b582
Daisie Huang <daisieh@gmail.com>
2025-04-14 12:14:14 -0700

    DIG-1993: add a pause after restart during ingest setup (#1026)
    * add a pause after restart
    
    * exec_with_expected exits with error if unexpected
    
    * check service-info instead

--- lib/candig-ingest/candig-ingest_setup.sh
+++ lib/candig-ingest/candig-ingest_setup.sh
@@ -25,6 +25,14 @@ docker restart $ingest
 # If present, create and authorize default site admin as CanDIG authorized user:
 site_admin_token=$(python site_admin_token.py)
 if [[ $CANDIG_SITE_ADMIN_USER != "" ]]; then
+  bash $PWD/exec_with_expected.sh "curl -si \"${CANDIG_URL}/ingest/service-info\" -H \"Authorization: Bearer ${site_admin_token}\"" "200 OK"
+  while [ $? -ne 0 ]
+  do
+    echo "..."
+      sleep 1
+      bash $PWD/exec_with_expected.sh "curl -si \"${CANDIG_URL}/ingest/service-info\" -H \"Authorization: Bearer ${site_admin_token}\"" "200 OK"
+  done
+
   echo ">> approving $CANDIG_SITE_ADMIN_USER as a CanDIG authorized user"
   bash $PWD/exec_with_expected.sh "curl -sX \"POST\" \"${CANDIG_URL}/ingest/user/pending/request\" -H \"Authorization: Bearer ${site_admin_token}\"" "$CANDIG_SITE_ADMIN_USER"
   bash $PWD/exec_with_expected.sh "curl -sX \"POST\" \"${CANDIG_URL}/ingest/user/pending/${CANDIG_SITE_ADMIN_USER}\" -H \"Authorization: Bearer ${site_admin_token}\"" "$CANDIG_SITE_ADMIN_USER"
