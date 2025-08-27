75f906a522d791bd2f60134874e8db1051f05a11
Marion <mshadbolt@users.noreply.github.com>
2025-08-27 16:12:56 -0700

    fix typo

--- lib/candig-api/ddl/load_synth_data.sql
+++ lib/candig-api/ddl/load_synth_data.sql
@@ -6,7 +6,7 @@
 \copy omop.concept_synonym FROM 'tmp/omopdata/concept_synonym.csv' WITH (FORMAT CSV, HEADER true);
 \copy omop.domain FROM 'tmp/omopdata/domain.csv' WITH (FORMAT CSV, HEADER true);
 \copy omop.relationship FROM 'tmp/omopdata/relationship.csv' WITH (FORMAT CSV, HEADER true);
-\copy omop.vocabulary FROM 'data/tmp/omopdata/Tutorial-ETL/vocabulary/vocabulary.csv' WITH (FORMAT CSV, HEADER true);
+\copy omop.vocabulary FROM 'tmp/omopdata/vocabulary.csv' WITH (FORMAT CSV, HEADER true);
 
 --Load synth data
 \copy omop.care_site FROM 'tmp/omopdata/care_site.csv' WITH (FORMAT CSV, HEADER true);
